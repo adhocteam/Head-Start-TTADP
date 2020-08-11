78a87764b5f0f13f1ff43c3af15d4f8a006292c2
Josh Salisbury <josh@adhocteam.us>
2020-08-11 11:39:55 -0500

    Remove docker on windows files

--- frontend/win-dev.ps1
+++
@@ -1,3 +0,0 @@
-docker build -t head-start-dev -f Dockerfile.win .
-docker run --privileged --network host --rm -d -v "$(pwd):/app" --name frontend-dev head-start-dev
-docker exec -it frontend-dev sh
