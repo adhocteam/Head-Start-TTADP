e8a68c96fe86e345a94bd0206a7095f44a9eaba7
kryswisnaskas <krystyna.wisnaskas@gmail.com>
2020-08-17 15:02:02 -0400

    Modified test

--- src/index.test.js
+++ src/index.test.js
@@ -3,8 +3,10 @@ import server from './index';
 
 describe('Root', () => {
   test('Returns a 200', async () => {
-    const response = await request(server).get('/');
+    const response = await request(server).get('/')
+      .expect('Content-Type', 'text/html; charset=utf-8');
     expect(response.status).toBe(200);
+    expect(response.text).toContain('Hello');
   });
   afterAll(async () => {
     await server.close();
