aa9bcb19d80cd723e8e588917b96b10cdc512dd6
kryswisnaskas <krystyna.wisnaskas@gmail.com>
2020-08-21 16:41:45 -0400

    Changed oauth2 client library to client-oauth2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import helmet from 'helmet';
-import passport from 'passport';
+import axios from 'axios';
 
-import authMiddleware from './middleware/authMiddleware';
+import authMiddleware, { hsesAuth } from './middleware/authMiddleware';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(passport.initialize());
 
 app.use(authMiddleware);
 
@@ -17,8 +16,32 @@ app.get('/', (req, res) => {
   res.send('Hello from ttadp');
 });
 
-app.get('/oauth2-client/login/oauth2/code/',
-  passport.authenticate('oauth2'));
+app.get('/auth/hses', (req, res) => {
+  const uri = hsesAuth.code.getUri();
+
+  res.redirect(uri);
+});
+
+app.get('/oauth2-client/login/oauth2/code/', async (req, res) => {
+  try {
+    const user = await hsesAuth.code.getToken(req.originalUrl);
+    // user will have accessToken and refreshToken
+    // console.log(user);
+    const requestObj = user.sign({
+      method: 'get',
+      url: 'https://uat.hsesinfo.org/auth/user/me',
+    });
+
+    const { url } = requestObj;
+
+    const response = await axios.get(url, requestObj);
+    const { data } = response;
+    const { authorities } = data;
+    res.send(authorities);
+  } catch (error) {
+    // console.log(error);
+  }
+});
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,29 +1,16 @@
-import passport from 'passport';
-import OAuth2Strategy from 'passport-oauth2';
+import ClientOAuth2 from 'client-oauth2';
 
 // TODO: move values to env variables
-const strategy = new OAuth2Strategy(
-  {
-    // state: true,
-    authorizationURL: 'https://uat.hsesinfo.org/auth/oauth/authorize',
-    tokenURL: 'https://uat.hsesinfo.org/auth/oauth/token',
-    clientID: '7b9268101989e6ce7efc',
-    clientSecret: 'ABHnjtF6iB1fsg940xBHvZy89Las7X31gywtsMChHrMV',
-    callbackURL: 'http://localhost:3000/api/oauth2-client/login/oauth2/code/',
-  },
-  (accessToken, refreshToken, profile, cb) => {
-    //User.findOrCreate({ exampleId: profile.id }, (err, user) => cb(err, user));
-  },
-);
-
-strategy.userProfile = function (accessToken, done) {
-  // access /user_info with accessToken
-};
-
-passport.use(strategy);
+export const hsesAuth = new ClientOAuth2({
+  clientId: '7b9268101989e6ce7efc',
+  clientSecret: 'ABHnjtF6iB1fsg940xBHvZy89Las7X31gywtsMChHrMV',
+  accessTokenUri: 'https://uat.hsesinfo.org/auth/oauth/token',
+  authorizationUri: 'https://uat.hsesinfo.org/auth/oauth/authorize',
+  redirectUri: 'http://localhost:8080/oauth2-client/login/oauth2/code/',
+  scopes: ['user_info'],
+});
 
 // Placeholder to add any auth logic
 export default async function authMiddleware(req, res, next) {
-  // Continue to the next middleware or ttdap
   next();
 }
