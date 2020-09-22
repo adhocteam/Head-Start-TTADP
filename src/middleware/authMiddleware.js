7446e2958987c093065371e7b8c1271200fbaa49
kryswisnaskas <krystyna.wisnaskas@gmail.com>
2020-09-22 10:48:50 -0400

    Make hses uris configuratble

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,9 +4,9 @@ import ClientOAuth2 from 'client-oauth2';
 export const hsesAuth = new ClientOAuth2({
   clientId: process.env.AUTH_CLIENT_ID,
   clientSecret: process.env.AUTH_CLIENT_SECRET,
-  accessTokenUri: 'https://uat.hsesinfo.org/auth/oauth/token',
-  authorizationUri: 'https://uat.hsesinfo.org/auth/oauth/authorize',
-  redirectUri: 'http://localhost:8080/oauth2-client/login/oauth2/code/',
+  accessTokenUri: `${process.env.AUTH_BASE}/auth/oauth/token`,
+  authorizationUri: `${process.env.AUTH_BASE}/auth/oauth/authorize`,
+  redirectUri: `${process.env.REDIRECT_URI_HOST}/oauth2-client/login/oauth2/code/`,
   scopes: ['user_info'],
 });
 
