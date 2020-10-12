1f9b03ddfb85e51a353c445914c9c37a05245648
Sarah-Jaine Szekeresh <sarah-jaine@adhocteam.us>
2020-10-12 14:11:00 -0500

    move state management infra to seperate main

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -5,54 +5,10 @@
 terraform {
   backend "s3" {
     bucket         = "ohs-terraform-state"
-    key            = "terraform.tfstate.dvp-dev"
+    key            = "terraform.tfstate.dev"
     dynamodb_table = "ohs-terraform-lock"
     encrypt        = true
   }
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "ohs-terraform-state"
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "ohs-terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
-###
-# Providers - service where infrastructure is instantiated
-###
-
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
-}
-
-provider "cloudfoundry" {
-  api_url      = var.cf_api_url
-  user         = var.cf_user
-  password     = var.cf_password
-  app_logs_max = 30
 }
 
 ###
