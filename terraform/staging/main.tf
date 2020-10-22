f7e8530684e6d5c06bc1da402566379b42008a30
Sarah-Jaine Szekeresh <sarah-jaine@adhocteam.us>
2020-10-22 11:12:43 -0500

    add prod, staging

---
+++ terraform/staging/main.tf
@@ -0,0 +1,55 @@
+###
+# Terraform settings and backend
+###
+
+terraform {
+  required_providers {
+    cloudfoundry = {
+      source  = "cloudfoundry-community/cloudfoundry"
+      version = "0.12.6"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "cg-77510827-41f7-4b13-aa6b-c292afe85b25"
+    key     = "terraform.tfstate.staging"
+    encrypt = true
+    region  = "us-gov-west-1"
+  }
+}
+
+provider "cloudfoundry" {
+  api_url      = var.cf_api_url
+  user         = var.cf_user
+  password     = var.cf_password
+  app_logs_max = 30
+}
+
+provider "aws" {
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+  region     = var.aws_region
+}
+
+###
+# Target space/org
+###
+
+data "cloudfoundry_space" "space" {
+  org_name = var.cf_org_name
+  name     = var.cf_space_name
+}
+
+###
+# RDS instance
+###
+
+data "cloudfoundry_service" "rds" {
+  name = "aws-rds"
+}
+
+resource "cloudfoundry_service_instance" "database" {
+  name         = "ttahub-${var.env}"
+  space        = data.cloudfoundry_space.space.id
+  service_plan = data.cloudfoundry_service.rds.service_plans["medium-psql"]
+}
