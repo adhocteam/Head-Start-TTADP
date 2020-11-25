959afbdbab9724fb831e85e667c68f166650fcf5
Sarah-Jaine Szekeresh <sarah-jaine@adhocteam.us>
2020-11-25 14:18:09 -0600

    add tests

--- src/middleware/authMiddleware.test.js
+++ src/middleware/authMiddleware.test.js
@@ -1,7 +1,20 @@
 import {} from 'dotenv/config';
+import { UNAUTHORIZED } from 'http-codes';
+
 import authMiddleware, { login } from './authMiddleware';
 
 describe('authMiddleware', () => {
+  const ORIGINAL_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules() // clear the cache
+    process.env = { ...ORIGINAL_ENV }; // make a copy
+  });
+
+  afterAll(() => {
+    process.env = ORIGINAL_ENV; // restore original env
+  });
+
   it('should allow access if user data is present', async () => {
     const mockNext = jest.fn();
     const mockSession = jest.fn();
@@ -51,5 +64,46 @@ describe('authMiddleware', () => {
     };
     login(mockRequest, mockResponse);
     expect(mockResponse.redirect).not.toHaveBeenCalledWith(process.env.TTA_SMART_HUB_URI);
+  });
+
+  it('bypass authorization if variables are set for UAT or accessibility testing', async () => {
+    // auth is bypassed if non-prod NODE_ENV and BYPASS_AUTH = 'true', needed for running cucumber and axe tests
+    process.env.NODE_ENV = 'development'
+    process.env.BYPASS_AUTH = 'true'
+    const mockNext = jest.fn();
+    const mockSession = jest.fn();
+    mockSession.userId = undefined;
+    const mockRequest = {
+      path: '/api/endpoint',
+      session: mockSession,
+    };
+    const mockResponse = {
+      redirect: jest.fn(),
+      sendStatus: jest.fn(),
+    };
+    await authMiddleware(mockRequest, mockResponse, mockNext);
+    expect(mockResponse.redirect).not.toHaveBeenCalled();
+    expect(mockNext).toHaveBeenCalled();
+  });
+
+  it('require authorization if variables are set NOT for UAT or accessibility testing', async () => {
+    // set env variables to values that require authorization
+    process.env.NODE_ENV = 'production'
+    process.env.BYPASS_AUTH = 'true'
+    const mockNext = jest.fn();
+    const mockSession = jest.fn();
+    mockSession.userId = undefined;
+    const mockRequest = {
+      path: '/api/endpoint',
+      session: mockSession,
+    };
+    const mockResponse = {
+      redirect: jest.fn(),
+      sendStatus: jest.fn(),
+    };
+    await authMiddleware(mockRequest, mockResponse, mockNext);
+    expect(mockResponse.redirect).not.toHaveBeenCalled();
+    expect(mockResponse.sendStatus).toHaveBeenCalledWith(UNAUTHORIZED);
+    expect(mockNext).not.toHaveBeenCalled();
   });
 });
