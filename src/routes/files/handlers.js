22a61c674558bd1b5532c5d4376b7f46743153e1
Chuck McAndrew <chuck.mcandrew@adhocteam.us>
2021-02-03 11:29:34 -0500

    fmt

--- src/routes/files/handlers.js
+++ src/routes/files/handlers.js
@@ -31,7 +31,6 @@ export const createFileMetaData = async (
     status: 'UPLOADING',
     fileSize,
   };
-  console.log(`newFile = ${JSON.stringify(newFile)}`)
   let file;
   try {
     file = await File.create(newFile);
