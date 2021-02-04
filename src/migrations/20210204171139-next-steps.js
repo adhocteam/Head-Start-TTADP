292a7c1aae3c7a1481a0cadb057a612067388190
Daniel Riquiac Gopar <danielriquiacgopar@danielrrmacbook.lan>
2021-02-04 11:28:41 -0800

    Add backend model

---
+++ src/migrations/20210204171139-next-steps.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('NextSteps', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+
+      activityReportId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'ActivityReports',
+          },
+        },
+      },
+
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'Users',
+          },
+          key: 'id',
+        },
+      },
+
+      note: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+
+      noteType: {
+        allowNull: false,
+        type: Sequelize.ENUM('SPECIALIST', 'GRANTEE'),
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('NextSteps');
+  },
+};
