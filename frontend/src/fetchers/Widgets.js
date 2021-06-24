478ad99a3e2b0ee29705626029c49953cf9487ca
Matt Bevilacqua <thewatermethod@gmail.com>
2021-06-24 16:40:13 -0400

    fixed appending Start Date to widgets query string

--- frontend/src/fetchers/Widgets.js
+++ frontend/src/fetchers/Widgets.js
@@ -3,7 +3,7 @@ import { get } from './index';
 
 const fetchWidget = async (widgetId, region, dateRange = '', query = '') => {
   const queryStr = query ? `?${query}&` : '?&';
-  const dateRangeStr = dateRange !== '' ? `&date=${dateRange}` : '';
+  const dateRangeStr = dateRange !== '' ? `&startDate.win=${dateRange}` : '';
   const res = await get(join('/', 'api', 'widgets', `${widgetId}${queryStr}region.in[]=${region}${dateRangeStr}`));
   return res.json();
 };
