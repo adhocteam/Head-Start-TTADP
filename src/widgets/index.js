7801fdc82e816c09182d9724bc8dcea7c415dc54
Matt Bevilacqua <thewatermethod@gmail.com>
2021-06-24 16:43:18 -0400

    post merge fix import

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,6 +1,7 @@
 import example from './example';
 import overview from './overview';
 import reasonList from './reasonList';
+import dashboardOverview from './dashboardOverview';
 
 /*
   All widgets need to be added to this object
@@ -9,4 +10,5 @@ export default {
   example,
   overview,
   reasonList,
+  dashboardOverview,
 };
