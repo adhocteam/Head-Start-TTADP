0febbb8c8c6188c5e8d0912d105fb4ad9eb616b6
Daniel Cloud <daniel.cloud@adhocteam.us>
2021-06-28 18:56:34 -0400

    Remove grants,grantees since there are seeders for those. Fix various syntax errors

--- src/seeders/20210610163414-fake-data.js
+++ src/seeders/20210610163414-fake-data.js
@@ -2,62 +2,47 @@ const faker = require('faker');
 
 const fakeRegionId = 404;
 
-function generateFakeGrantee() {
-  // Grantee has .name, belongs to many Goals and has many Grants
-  return {
-    name: faker.company.companyName,
-  };
-}
-
-function generateFakeGrant({ granteeId, regionId = fakeRegionId }) {
-  return {
-    number: faker.datatype.uuid(),
-    cdi: faker.datatype.boolean(),
-    status: faker.arrayElement(['Active', 'Inactive']),
-    startDate: faker.date.past(2),
-    endDate: faker.date.future(2),
-    name: faker.company.companyName(),
-    granteeId,
-    regionId,
-  };
-}
-
 function generateFakeGoal() {
   return {
-    name: faker.hacker.phrase(),
+    name: faker.lorem.sentence(),
     status: faker.random.arrayElement(['Not started', 'In progress', 'Completed', 'Dropped']),
-    timeframe: faker.hacker.verb(), // TODO: fake realistic values,
-    isFromSmartsheetTtaPlan: faker.random.boolean(),
+    timeframe: faker.lorem.words(), // TODO: fake realistic values,
+    isFromSmartsheetTtaPlan: faker.datatype.boolean(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
   };
 }
 
-function generateFakeObjective({ goalId = null }) {
+function generateFakeObjective({ goalId = null } = {}) {
   return {
-    title: faker.hacker.phrase(),
+    title: faker.lorem.sentence(),
     ttaProvided: faker.random.arrayElement(['Training', 'Technical Assistance', 'Test Data']),
     status: faker.random.arrayElement(['Not started', 'In progress', 'Completed', 'Dropped']),
     goalId,
+    createdAt: new Date(),
+    updatedAt: new Date(),
   };
 }
 
-function generateFakeActivityReport({ regionId = fakeRegionId, status = 'Fake!' }) {
+function generateFakeActivityReport({ regionId = fakeRegionId, status = 'draft' } = {}) {
   return {
-    legacyId: '',
     userId: null,
     lastUpdatedById: null,
     approvingManagerId: null,
     ECLKCResourcesUsed: [Array.from({ length: 10 }).map(() => faker.internet.url())],
     nonECLKCResourcesUsed: [Array.from({ length: 4 }).map(() => faker.internet.url())],
     additionalNotes: faker.lorem.lines(),
-    numberOfParticipants: faker.datatypes.number(500),
+    numberOfParticipants: faker.datatype.number(500),
     deliveryMethod: faker.helpers.randomize(['a', 'b']),
-    duration: faker.datatypes.number({ min: 0.5, max: 40.0, precision: 0.01 }),
+    duration: faker.datatype.number({ min: 0.5, max: 40.0, precision: 0.01 }),
     endDate: faker.date.future(0.25), // date in near future
     startDate: faker.date.past(0.25), // date in recent past
-    activityRecipientType: faker.randomize(['grantee', 'non-grantee']),
+    activityRecipientType: faker.random.arrayElement(['grantee', 'non-grantee']),
     requester: faker.random.arrayElement(['grantee', 'regionalOffice']),
     regionId,
     status,
+    createdAt: new Date(),
+    updatedAt: new Date(),
   };
 }
 
@@ -69,27 +54,21 @@ module.exports = {
     };
     await queryInterface.bulkInsert('Regions', [fakeRegion]);
 
-    // Generate fake grantees
-    const fakeGrantees = Array.from({ length: 100 }).map(() => generateFakeGrantee);
-    const savedGrantees = await queryInterface.bulkInsert('Grantees', fakeGrantees);
-    // Generate 1 grant for each grantee
-    const fakeGrants = savedGrantees.map((grantee) => generateFakeGrant({ granteeId: grantee.id }));
-    await queryInterface.bulkInsert('Grants', fakeGrants);
-
     // Generate fake goals
-    const fakeGoals = Array.from({ length: 300 }).map(() => generateFakeGoal);
-    const savedGoals = await queryInterface.bulkInsert('Goals', fakeGoals);
+    const fakeGoals = Array.from({ length: 300 }).map(generateFakeGoal);
+    const savedGoals = await queryInterface.bulkInsert('Goals', fakeGoals, { returning: true });
 
     // For each Goal, generate some objectives
     const fakeObjectives = savedGoals.flatMap((goal) => {
       const num = faker.datatype.number({ min: 1, max: 10 });
       return Array.from({ length: num }).map(() => generateFakeObjective(goal.id));
     });
-    const savedObjectives = await queryInterface.bulkInsert('Objectives', fakeObjectives);
+    const savedObjectives = await queryInterface.bulkInsert('Objectives', fakeObjectives, { returning: true });
 
     // Generate fake ARs
-    const fakeActivityReports = Array.from({ length: 100 }).map(() => generateFakeActivityReport());
-    const savedARs = await queryInterface.bulkInsert('ActivityReports', fakeActivityReports);
+    const fakeActivityReports = Array.from({ length: 100 }).map(generateFakeActivityReport);
+    const savedARs = await queryInterface.bulkInsert('ActivityReports', fakeActivityReports, { returning: true });
+    console.log(`Created ${savedARs.length} Activity Reports`);
 
     // Attach some objectives to ARs
     const fakeARObjectives = savedARs.flatMap((ar) => {
@@ -97,12 +76,15 @@ module.exports = {
       const randomObjectives = faker.random.arrayElements(savedObjectives, numObjectives);
       return randomObjectives.map((o) => ({ objectiveId: o.id, activityReportId: ar.id }));
     });
-    await queryInterface.bulkInsert('ActivityReportObjectives', fakeARObjectives);
+    const savedARObjectives = await queryInterface.bulkInsert('ActivityReportObjectives', fakeARObjectives, { returning: true });
+    return savedARObjectives;
   },
 
   // down: async (queryInterface, Sequelize) => {
   //   /**
   //    * Add commands to revert seed here.
+  //    *
+  //    * Delete anything in fakeRegion?
   //    *
   //    * Example:
   //    * await queryInterface.bulkDelete('People', null, {});
