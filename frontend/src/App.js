f226ec307be51e085df4822769efe9d3b7066709
Matt Bevilacqua <thewatermethod@gmail.com>
2021-07-07 13:41:16 -0400

    finesse where we land and where we dont

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ import Admin from './pages/Admin';
 import Dashboard from './pages/Dashboard';
 import Unauthenticated from './pages/Unauthenticated';
 import NotFound from './pages/NotFound';
-import Home from './pages/Home';
+// import Home from './pages/Home';
 import Landing from './pages/Landing';
 import ActivityReport from './pages/ActivityReport';
 import LegacyReport from './pages/LegacyReport';
@@ -30,6 +30,7 @@ import LandingLayout from './components/LandingLayout';
 import RequestPermissions from './components/RequestPermissions';
 import AriaLiveContext from './AriaLiveContext';
 import AriaLiveRegion from './components/AriaLiveRegion';
+import { SCOPE_IDS } from './Constants';
 
 function App() {
   const [user, updateUser] = useState();
@@ -81,8 +82,13 @@ function App() {
   // does a user have permissions to write or approve reports?
   // include write or approve, this would be true
   // otherwise, it'll return false
-  // const startOnActivityReport = (currentUser) => currentUser.permissions.find((permission) => {
-  // permission.scopeId === 3 || permission.scopeId === 5});
+
+  function startOnActivityReport(currentUser) {
+    return currentUser.permissions.find((permission) => (
+      // eslint-disable-next-line max-len
+      permission.scopeId === SCOPE_IDS.READ_WRITE_ACTIVITY_REPORTS || permission.scopeId === SCOPE_IDS.APPROVE_ACTIVITY_REPORTS
+    ));
+  }
 
   const admin = isAdmin(user);
   const enableWidgets = process.env.REACT_APP_ENABLE_WIDGETS === 'true';
@@ -113,18 +119,19 @@ function App() {
         <Route
           exact
           path="/"
-          render={() => <Home />}
-          //   const showReport = startOnActivityReport(user);
+          render={(match) => {
+            const showReport = startOnActivityReport(user);
 
-          //   if (showReport) {
-          //     return (
-          //       <LandingLayout><Landing match={match} /></LandingLayout>
-          //     );
-          //   }
+            if (showReport) {
+              return (
+                <LandingLayout><Landing match={match} /></LandingLayout>
+              );
+            }
 
-          //   return (
-          //     <Dashboard user={user} />
-          //   );
+            return (
+              <Dashboard user={user} />
+            );
+          }}
         />
         <Route
           exact
