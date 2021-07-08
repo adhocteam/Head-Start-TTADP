824394df43d9b1323e9d4fcafa6264865bcbeb2e
Matt Bevilacqua <thewatermethod@gmail.com>
2021-07-08 13:36:10 -0400

    comment and add to scheduled job

--- src/lib/updateTopicNames.js
+++ src/lib/updateTopicNames.js
@@ -1,6 +1,9 @@
 import { Op } from 'sequelize';
 import { ActivityReport } from '../models';
 
+/**
+ * put it in this object so we could add to this as necessary
+ */
 const TOPIC_DICTIONARY = [
   {
     old: 'Behavioral / Mental Health',
@@ -25,6 +28,7 @@ const TOPIC_DICTIONARY = [
 ];
 
 export default async function updateTopicNames() {
+  // we find any activity reports that have ANY of the old topics in their topics field
   const reports = await ActivityReport.findAll({
     where: {
       topics: {
@@ -33,23 +37,34 @@ export default async function updateTopicNames() {
     },
   });
 
+  // this will hold an array of promises which I
+  // frankly am only using to get the unit test timing to work out
   const promises = [];
 
+  // loop through the found reports
   reports.forEach(async (report) => {
+    // copy existing state
     const topics = [...report.topics];
 
+    // within each report, check our array of topics to rename
     TOPIC_DICTIONARY.forEach((topic) => {
+      // find the index of the old topic, if it exists
       const index = topics.indexOf(topic.old);
 
+      // -1 if it doesn't exist (Thanks Javascript)
       if (index !== -1) {
         // eslint-disable-next-line no-console
         console.log(`Renaming ${topic.old} to ${topic.renamed} in ${report.id}`);
+        // mutate our copy
         topics.splice(index, 1, topic.renamed);
       }
     });
 
+    // push our update operation to our promises array
     promises.push(report.update({ topics }));
   });
 
+  // return our updates as an array of promises
+  // (will return an array of updated Activity Report objects)
   return Promise.all(promises);
 }
