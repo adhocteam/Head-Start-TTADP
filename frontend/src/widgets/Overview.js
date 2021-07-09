20043919b26cae9a63c7143f9d688f4e16a2b5b7
kryswisnaskas <krystyna.wisnaskas@gmail.com>
2021-07-09 11:12:33 -0400

    Update text on non-grantees served

--- frontend/src/widgets/Overview.js
+++ frontend/src/widgets/Overview.js
@@ -57,7 +57,7 @@ function Overview({ data, region }) {
       </Grid>
       <Grid row gap className="smart-hub--overview-data">
         <Field col="fill" tablet={{ col: true }} label="Grants served " data={data.numGrants} />
-        <Field col="fill" label="Non-grantees served" data={data.numNonGrantees} />
+        <Field col="fill" label="Non-grantee entities served" data={data.numNonGrantees} />
         <Field col="fill" label="Activity reports" data={data.numReports} />
         <Field col="fill" label="Participants" data={data.numParticipants} />
         <Field col={2} label="Hours of TTA" data={data.sumDuration} />
